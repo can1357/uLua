357545f83184bc00bc3d990d89bc7b8801401cc1
can1357 <me@can.ac>
2022-03-12 22:00:30 +0300

    Faster cdata, remove tags.

--- includes/ulua/ffi.hpp
+++ includes/ulua/ffi.hpp
@@ -6,8 +6,8 @@
 #include "state.hpp"
 #include "userdata.hpp"
 
-#ifndef ULUA_CTYPE_TAGGED
-	#define ULUA_CTYPE_TAGGED 0
+#ifndef ULUA_CTYPE_VALIDATED
+	#define ULUA_CTYPE_VALIDATED 0
 #endif
 
 #if ULUA_JIT
@@ -58,22 +58,11 @@ namespace ulua
 	{
 		inline static uint32_t make_tag() { return ( uint32_t ) ( uint64_t ) &__userdata_tag<std::remove_const_t<T>>; }
 
-#if ULUA_CTYPE_TAGGED
-		uint32_t                       tag;
 		mutable std::remove_const_t<T> storage;
-
-		template<typename... Tx>
-		userdata_wrapper( Tx&&... args ) : tag( make_tag() ), storage( std::forward<Tx>( args )... ) {}
-		inline constexpr bool check_type() const { return tag == make_tag(); }
-#else
-
-		mutable std::remove_const_t<T> storage;
-
 		template<typename... Tx>
 		constexpr userdata_wrapper( Tx&&... args ) : storage( std::forward<Tx>( args )... ) {}
-		inline constexpr bool check_type() const { return true; }
-#endif
 
+		inline constexpr bool check_type() const { return true; }
 		inline constexpr bool check_qual() const { return true; }
 
 		inline constexpr T* get() const { return &storage; }
@@ -99,20 +88,80 @@ namespace ulua
 		userdata_by_pointer() = delete;
 	};
 
+	// Ctype cache.
+	//
+	template<typename T>
+	struct ctype_id_cache
+	{
+		using meta =  userdata_metatable<T>;
+
+		inline static std::atomic<uint64_t> value = {};
+
+		inline static void write( lua_State* L, CTypeID in )
+		{
+			uint64_t v = uint32_t( ( uint64_t ) L );
+			v |= uint64_t( in ) << 32;
+			value.exchange( v );
+		}
+		inline static bool read( lua_State* L, CTypeID& out )
+		{
+			uint64_t v = value.load( std::memory_order::relaxed );
+			if ( uint32_t( v ) != uint32_t( ( uint64_t ) L ) ) [[unlikely]]
+				return false;
+			out = uint32_t( v >> 32 );
+			return true;
+		}
+		ULUA_COLD static CTypeID fetch_slow( lua_State* L )
+		{
+			CTypeID type_id;
+			if ( stack::create_metatable( L, userdata_mt_name<T>().data() ) ) [[unlikely]]
+			{
+				// Create the C definition.
+				if constexpr ( CTypeHasInlineCDef<user_traits<T>> )
+					ffi::cdef( L, user_traits<T>::cdef ).assert();
+				// Get the type ID.
+				type_id = ffi::typeid_of( L, userdata_name<T>().data() );
+				// Setup the metatable.
+				meta::setup( L, stack::top_t{} );
+				// Add the __cid property.
+				ulua::table table{ L, stack::top_t{} };
+				table[ "i" ] = type_id;
+				// Set the type metatable.
+				ffi::set_metatable( L, userdata_name<T>().data(), table );
+			}
+			else
+			{
+				// Read the __cid property.
+				ulua::stack_table table{ L, stack::top_t{} };
+				type_id = table[ "i" ];
+			}
+			// Update cache.
+			write( L, type_id );
+			return type_id;
+		}
+		ULUA_INLINE static CTypeID fetch( lua_State* L )
+		{
+			CTypeID type_id;
+			if ( !read( L, type_id ) ) [[unlikely]]
+				type_id = fetch_slow( L );
+			return type_id;
+		}
+	};
+
 	// Replace type traits of the userdata.
 	//
 	template<typename T> requires UserCType<std::remove_const_t<T>>
 	struct type_traits<userdata_wrapper<T>>
 	{
+		using cache = ctype_id_cache<std::remove_const_t<T>>;
+
 		// No pusher.
 		ULUA_INLINE static bool check( lua_State* L, int& idx )
 		{ 
 			auto* tv = accel::ref( L, idx++ );
 			if ( !tviscdata( tv ) )
 				return false;
-			auto* cd = cdataV( tv );
-			auto* wr = ( userdata_wrapper<T>* ) cdataptr( cd );
-			return wr->check_type();
+			return cdataV( tv )->ctypeid == cache::fetch( L );
 		}
 		ULUA_INLINE static userdata_wrapper<T>& get( lua_State* L, int& idx )
 		{
@@ -121,85 +170,25 @@ namespace ulua
 				type_error( L, idx - 1, userdata_name<std::remove_const_t<T>>().data() );
 			auto* cd = cdataV( tv );
 			auto* wr = ( userdata_wrapper<T>* ) cdataptr( cd );
-			if ( !wr->check_type() )
+
+#if ULUA_CTYPE_VALIDATED
+			if ( cd->ctypeid != cache::fetch( L ) )
 				type_error( L, idx - 1, userdata_name<std::remove_const_t<T>>().data() );
+#endif
 			return *wr;
 		}
 	};
-
-	template<typename T>
-	struct ctype_id_cache
-	{
-		inline static std::atomic<uint64_t> value = {};
-
-		inline static void write( lua_State* L, CTypeID in )
-		{
-			uint64_t v = in;
-			v |= ( uint64_t( L ) >> 4 ) << 32;
-			value.exchange( v );
-		}
-		inline static bool read( lua_State* L, CTypeID& out )
-		{
-			uint64_t v = value.load( std::memory_order::relaxed );
-			if ( ( v >> 32 ) == uint32_t( uint64_t( L ) >> 4 ) )
-			{
-				out = uint32_t( v );
-				return true;
-			}
-			return false;
-		}
-	};
-	template<typename T>
-	struct ctype_id_cache<const T> : ctype_id_cache<T> {};
-
 	template<typename T> requires UserCType<std::remove_const_t<T>>
 	struct user_type_traits<T> : emplacable_tag_t
 	{
-		using meta = userdata_metatable<std::remove_const_t<T>>;
-		using cache = ctype_id_cache<T>;
-
-		ULUA_COLD static CTypeID fetch_type_id_slow( lua_State* L )
-		{
-			CTypeID type_id;
-			if ( stack::create_metatable( L, userdata_mt_name<std::remove_const_t<T>>().data() ) ) [[unlikely]]
-			{
-				// Create the C definition.
-				if constexpr( CTypeHasInlineCDef<user_traits<std::remove_const_t<T>>> )
-					ffi::cdef( L, user_traits<std::remove_const_t<T>>::cdef ).assert();
-				// Get the type ID.
-				type_id = ffi::typeid_of( L, userdata_name<std::remove_const_t<T>>().data() );
-				// Setup the metatable.
-				meta::setup( L, stack::top_t{} );
-				// Add the __cid property.
-				ulua::table table{ L, stack::top_t{} };
-				table[ "__cid" ] = type_id;
-				// Set the type metatable.
-				ffi::set_metatable( L, userdata_name<std::remove_const_t<T>>().data(), table );
-			}
-			else
-			{
-				// Read the __cid property.
-				ulua::stack_table table{ L, stack::top_t{} };
-				type_id = table[ "__cid" ];
-			}
-			// Update cache.
-			cache::write( L, type_id );
-			return type_id;
-		}
+		using cache = ctype_id_cache<std::remove_const_t<T>>;
 
 		template<typename... Tx>
 		ULUA_INLINE static int emplace( lua_State* L, Tx&&... args )
 		{
-			// Get the C type ID.
-			//
-			CTypeID type_id;
-			if ( !cache::read( L, type_id ) ) [[unlikely]]
-				type_id = fetch_type_id_slow( L );
-
 			// Emplace the C type.
 			//
-			auto* cts = ctype_cts( L );
-			auto* cd = lj_cdata_new( cts, type_id, sizeof( userdata_wrapper<T> ) );
+			auto* cd = lj_cdata_new_( L, cache::fetch( L ), sizeof( userdata_wrapper<T> ) );
 			setcdataV( L, L->top, cd );
 			incr_top( L );
 			new ( cdataptr( cd ) ) userdata_wrapper<T>( std::forward<Tx>( args )... );
